5c682f85a712a6f58fc32d0a5da7d5cef16ce368
asela <123azeela@gmail.com>
2019-05-28 23:21:59 +0530

    changed db

--- db/connection.js
+++ db/connection.js
@@ -4,13 +4,13 @@ var mysql = require('mysql');
 
 //local mysql db connection
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'mysql',
-    database : 'locations_app'
+    host: 'us-cdbr-iron-east-02.cleardb.net',
+    user: 'bfb1858ff5134e',
+    password: '2f8b2348',
+    database: 'heroku_47526a6979522fb'
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
 });
 
